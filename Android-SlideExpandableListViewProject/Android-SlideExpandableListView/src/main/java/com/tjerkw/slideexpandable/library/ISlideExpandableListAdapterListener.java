9af79201cefc570af2d5e8af77c759346b5fb171
Lucas Ihlström <lucas@apping.se>
2013-11-19 13:37:14 +0100

    Removed all files.

--- Android-SlideExpandableListViewProject/Android-SlideExpandableListView/src/main/java/com/tjerkw/slideexpandable/library/ISlideExpandableListAdapterListener.java
+++
@@ -1,5 +0,0 @@
-package com.tjerkw.slideexpandable.library;
-
-public interface ISlideExpandableListAdapterListener {
-    public void actionButtonPressed(AbstractSlideExpandableListAdapter listAdapter, int position, int actionButtonID);
-}
